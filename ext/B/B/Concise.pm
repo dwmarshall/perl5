7c66a8ccbfecd2f2969950b80c443fedb12f3dc1
Nicholas Clark <nick@ccl4.org>
2021-04-26 11:05:21 +0000

    B::Concise now handles n-at-a-time for.

--- ext/B/B/Concise.pm
+++ ext/B/B/Concise.pm
@@ -14,7 +14,7 @@ use warnings; # uses #3 and #4, since warnings uses Carp
 
 use Exporter (); # use #5
 
-our $VERSION   = "1.004";
+our $VERSION   = "1.005";
 our @ISA       = qw(Exporter);
 our @EXPORT_OK = qw( set_style set_style_standard add_callback
 		     concise_subref concise_cv concise_main
@@ -853,9 +853,14 @@ sub concise_op {
 	# targ holds a reference count
         my $refs = "ref" . ($h{targ} != 1 ? "s" : "");
         $h{targarglife} = $h{targarg} = "$h{targ} $refs";
-    } elsif ($h{targ}) {
+    } elsif ($h{targ} && $h{name} ne 'iter') {
+        # for my ($q, $r, $s) () {} syntax hijacks the targ of the iter op,
+        # (which is the ->next of the enteriter) hence the special cases above
+        # and just below:
 	my $count = $h{name} eq 'padrange'
             ? ($op->private & $B::Op_private::defines{'OPpPADRANGE_COUNTMASK'})
+            : $h{name} eq 'enteriter'
+            ? $op->next->targ + 1
             : 1;
 	my (@targarg, @targarglife);
 	for my $i (0..$count-1) {
